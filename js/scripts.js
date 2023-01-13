6804808ecf017b013548f3c666a46ffa901f26c6
Ryan1806 <121129037+Ryan1806@users.noreply.github.com>
2023-01-13 10:13:05 -0300

    terminei

--- js/scripts.js
+++ js/scripts.js
@@ -142,6 +142,45 @@ function eachBoxes(type, boxes) {
     }
 }
 
+//scrol pra secoes
+let navBtn = $('.nav-item');
+
+let bannerSection = $('#mainSlider');
+let aboutSection = $('#about-area');
+let servicesSection = $('#sevices-area');
+let teamSection = $('#team-area');
+let portfolioSection = $('#portfolio-area');
+let contactSection = $('#contact-area');
+
+let scrollTo = '';
+
+$(navBtn).click(function() {
+
+  let btnId = $(this).attr('id');
+  console.log(btnId);
+
+  if(btnId == 'about-menu') {
+    scrollTo = aboutSection;
+  }else if(btnId == 'services-menu') {
+    scrollTo = servicesSection;
+  }else if(btnId == 'team-menu')
+ {
+  scrollTo = teamSection;
+ }else if(btnId == 'portfolio-menu') {
+  scrollTo = portfolioSection;
+ }else if(btnId == 'contact-menu') {
+  scrollTo = contactSection
+ }else {
+  scrollTo = bannerSection;
+ }
+
+ $([document.documentElement, document.body]).animate({
+  scrollTop: $(scrollTo).offset().top - 70
+}, 1500)
+
+  });
+
+
 });
 
 
